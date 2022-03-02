10a47820d1392f1e70d4d480e784fa5e7ad5af58
wasp93 <58074409+wasp93@users.noreply.github.com>
2022-03-02 19:21:53 +0100

    project initializing

---
+++ wasp-movie-library/src/main/java/com/wasp/movielibrary/waspmovielibrary/WaspMovieLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.wasp.movielibrary.waspmovielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaspMovieLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaspMovieLibraryApplication.class, args);
+	}
+
+}
---
+++ wasp-movie-library/src/test/java/com/wasp/movielibrary/waspmovielibrary/WaspMovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasp.movielibrary.waspmovielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaspMovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
